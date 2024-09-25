40c52d8a419ebaa6fd890cbd51ac62f7e4bf5718
4ustintr <petterpacker2@gmail.com>
2024-09-25 15:54:32 +0700

    update code

---
+++ DAY_05/install_react_app/src/App.js
@@ -0,0 +1,13 @@
+import ToDoList from '../src/TH_01_2024.09.25_PM/component/Todolist';
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
